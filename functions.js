52dcebc7e19015d7cac88af2b3707dd9359efad8
jdgvc3 <juandiegovazquezcabrera@gmail.com>
2022-07-27 11:39:58 -0500

    saving results

---
+++ functions.js
@@ -0,0 +1,7 @@
+function fn(){
+    console.log('hello from the fn function')
+}
+
+module.exports = {
+    fn
+}
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+const { fn } = require('./functions')
 console.log('hello from a file for a test respository');
+fn()
